8875a486a9c869b3348bc3d731fc60a4655794c3
imlonghao <git@imlonghao.com>
2022-02-23 00:54:58 +0800

    fix domain modifier not support

--- dnsconfig.js
+++ dnsconfig.js
@@ -2,13 +2,12 @@ var REG_NONE = NewRegistrar('none', 'NONE');
 var POWERDNS = NewDnsProvider('powerdns', 'POWERDNS', { 'default_ns': ['ns.imlonghao.dn42.'] });
 
 DEFAULTS(
-  REG_NONE,
   DnsProvider(POWERDNS),
   DefaultTTL(3600),
   NAMESERVER_TTL(86400)
 );
 
-D('imlonghao.dn42',
+D('imlonghao.dn42', REG_NONE,
   A('anycast', '172.22.68.0'),
   A('us1', '172.22.68.1'),
   A('sg1', '172.22.68.2'),
@@ -18,7 +17,7 @@ D('imlonghao.dn42',
   A('ns', '172.22.68.8')
 );
 
-D(REV('172.22.68.0/27'),
+D(REV('172.22.68.0/27'), REG_NONE,
   PTR('0', 'anycast.imlonghao.dn42.'),
   PTR('1', 'us1.imlonghao.dn42.'),
   PTR('2', 'sg1.imlonghao.dn42.'),
@@ -28,7 +27,7 @@ D(REV('172.22.68.0/27'),
   PTR('8', 'ns.imlonghao.dn42.')
 );
 
-D(REV('fd21:5c0c:9b7e::/48'),
+D(REV('fd21:5c0c:9b7e::/48'), REG_NONE,
   PTR('fd21:5c0c:9b7e::8', 'ns.imlonghao.dn42.'),
   PTR('fd21:5c0c:9b7e:1::', 'us1.imlonghao.dn42.'),
   PTR('fd21:5c0c:9b7e:2::', 'sg1.imlonghao.dn42.'),
