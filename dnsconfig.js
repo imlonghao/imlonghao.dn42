5b9973cb5554117b22a58a940abdd7d9d919ed51
imlonghao <git@imlonghao.com>
2022-02-23 00:50:43 +0800

    add fd21:5c0c:9b7e::/48

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,5 +1,5 @@
 var REG_NONE = NewRegistrar('none', 'NONE');
-var POWERDNS = NewDnsProvider('powerdns', 'POWERDNS', {'default_ns': ['ns.imlonghao.dn42.']});
+var POWERDNS = NewDnsProvider('powerdns', 'POWERDNS', { 'default_ns': ['ns.imlonghao.dn42.'] });
 
 D('imlonghao.dn42', REG_NONE, DnsProvider(POWERDNS),
   DefaultTTL(3600),
@@ -23,4 +23,15 @@ D(REV('172.22.68.0/27'), REG_NONE, DnsProvider(POWERDNS),
   PTR('4', 'de1.imlonghao.dn42.'),
   PTR('5', 'us2.imlonghao.dn42.'),
   PTR('8', 'ns.imlonghao.dn42.')
+);
+
+D(REV('fd21:5c0c:9b7e::/48'), REG_NONE, DnsProvider(POWERDNS),
+  DefaultTTL(3600),
+  NAMESERVER_TTL(86400),
+  PTR('fd21:5c0c:9b7e::8', 'ns.imlonghao.dn42.'),
+  PTR('fd21:5c0c:9b7e:1::', 'us1.imlonghao.dn42.'),
+  PTR('fd21:5c0c:9b7e:2::', 'sg1.imlonghao.dn42.'),
+  PTR('fd21:5c0c:9b7e:3::', 'hk1.imlonghao.dn42.'),
+  PTR('fd21:5c0c:9b7e:4::', 'de1.imlonghao.dn42.'),
+  PTR('fd21:5c0c:9b7e:5::', 'us2.imlonghao.dn42.')
 );
