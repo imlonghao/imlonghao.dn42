14e904969143c0a1acff7f9e517f2c4910ccf61a
imlonghao <github@esd.cc>
2025-07-07 22:56:26 +0800

    imlonghao.dn42: update IPv6

--- domains/imlonghao.js
+++ domains/imlonghao.js
@@ -28,13 +28,13 @@ D(REV('172.22.68.0/27'), REG_NONE
 
 D(REV('fd21:5c0c:9b7e::/48'), REG_NONE
   , PTR('fd21:5c0c:9b7e::8', 'ns.imlonghao.dn42.')
-  , PTR('fd21:5c0c:9b7e:1::', 'us1.imlonghao.dn42.')
-  , PTR('fd21:5c0c:9b7e:2::', 'sg1.imlonghao.dn42.')
-  , PTR('fd21:5c0c:9b7e:3::', 'hk1.imlonghao.dn42.')
-  , PTR('fd21:5c0c:9b7e:4::', 'de1.imlonghao.dn42.')
-  , PTR('fd21:5c0c:9b7e:5::', 'us2.imlonghao.dn42.')
-  , PTR('fd21:5c0c:9b7e:6::', 'no1.imlonghao.dn42.')
-  , PTR('fd21:5c0c:9b7e:7::', 'home.imlonghao.dn42.')
-  , PTR('fd21:5c0c:9b7e:9::', 'au1.imlonghao.dn42.')
+  , PTR('fd21:5c0c:9b7e:1::1', 'us1.imlonghao.dn42.')
+  , PTR('fd21:5c0c:9b7e:2::1', 'sg1.imlonghao.dn42.')
+  , PTR('fd21:5c0c:9b7e:3::1', 'hk1.imlonghao.dn42.')
+  , PTR('fd21:5c0c:9b7e:4::1', 'de1.imlonghao.dn42.')
+  , PTR('fd21:5c0c:9b7e:5::1', 'us2.imlonghao.dn42.')
+  , PTR('fd21:5c0c:9b7e:6::1', 'no1.imlonghao.dn42.')
+  , PTR('fd21:5c0c:9b7e:7::1', 'home.imlonghao.dn42.')
+  , PTR('fd21:5c0c:9b7e:9::1', 'au1.imlonghao.dn42.')
   , PTR('fd21:5c0c:9b7e:10::1', 'jp1.imlonghao.dn42.')
 );
